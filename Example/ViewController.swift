5f7bf53a5ce345406bf3e345887673a672e3b971
yoheimuta <yoheimuta@gmail.com>
2019-09-12 11:25:33 +0900

    chore: initialize

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by YOSHIMUTA YOHEI on 2019/09/12.
+//  Copyright © 2019 YOSHIMUTA YOHEI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RxMusicPlayer/RxMusicPlayer.h
@@ -0,0 +1,19 @@
+//
+//  RxMusicPlayer.h
+//  RxMusicPlayer
+//
+//  Created by YOSHIMUTA YOHEI on 2019/09/12.
+//  Copyright © 2019 YOSHIMUTA YOHEI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxMusicPlayer.
+FOUNDATION_EXPORT double RxMusicPlayerVersionNumber;
+
+//! Project version string for RxMusicPlayer.
+FOUNDATION_EXPORT const unsigned char RxMusicPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxMusicPlayer/PublicHeader.h>
+
+
