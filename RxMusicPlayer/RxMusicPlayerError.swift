6b8f1e30585ba7fddce776ae1f9811c9188e5612
yoheimuta <yoheimuta@gmail.com>
2019-10-01 18:08:17 +0900

    feat: Handle the metadata loading error

--- RxMusicPlayer/RxMusicPlayerError.swift
+++ RxMusicPlayer/RxMusicPlayerError.swift
@@ -10,11 +10,19 @@ import AVFoundation
 import Foundation
 
 public enum RxMusicPlayerError: Error, Equatable {
+    // Indicates an internal error. This implies a library bug.
     case internalError(String)
+    // Indicates an invalid weak reference. This implies a library bug.
     case notFoundWeakReference
+    // Indicates a given command is invalid.
     case invalidCommand(cmd: RxMusicPlayer.Command)
+    // Indicates a failure happended during the loading of a metadata.
+    case playerItemMetadataFailed(err: Error)
+    // Indicates a failure happended during the loading of an item.
     case playerItemFailed(err: Error)
+    // Indicates an AVPlayerItemNewErrorLogEntry.
     case playerItemError(log: AVPlayerItemErrorLog)
+    // Indicates an AVPlayerItemFailedToPlayToEndTimeError.
     case failedToPlayToEndTime(String)
 
     public static func == (lhs: RxMusicPlayerError, rhs: RxMusicPlayerError) -> Bool {
