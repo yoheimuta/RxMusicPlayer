bbe766d37a4a21361b64a9a233d4197a5a2acfdd
yoheimuta <yoheimuta@gmail.com>
2021-06-18 09:03:58 +0900

    demo: Create a demo app built on swiftui

---
+++ ExampleSwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ExampleSwiftUI
+//
+//  Created by Yoheimuta on 2021/06/15.
+//  Copyright © 2021 YOSHIMUTA YOHEI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExampleSwiftUI/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  ControlModel.swift
+//  ExampleSwiftUI
+//
+//  Created by Yoheimuta on 2021/06/15.
+//  Copyright © 2021 YOSHIMUTA YOHEI. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+final class ControlModel: ObservableObject {
+}
