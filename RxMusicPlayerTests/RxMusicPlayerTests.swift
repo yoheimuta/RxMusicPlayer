87122a4238dcaf9c5effd333a4f4435e5510b326
yoheimuta <yoheimuta@gmail.com>
2021-12-12 12:09:01 +0900

    tests: Add test cases about displayName() extended in CMTime

--- RxMusicPlayerTests/RxMusicPlayerTests.swift
+++ RxMusicPlayerTests/RxMusicPlayerTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import AVFoundation
 @testable import RxMusicPlayer
 
 class RxMusicPlayerTests: XCTestCase {
@@ -17,6 +18,16 @@ class RxMusicPlayerTests: XCTestCase {
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testCMTime_displayName() {
+        XCTAssertEqual(CMTimeMake(value: 1, timescale: 1).displayTime, "0:01", "convert an integer second")
+        XCTAssertEqual(CMTimeMake(value: 1, timescale: 10).displayTime, "0:00", "convert a non-integer second near 0")
+        XCTAssertEqual(CMTimeMake(value: 6, timescale: 10).displayTime, "0:01", "convert a non-integer second near 1")
+        XCTAssertEqual(CMTimeMake(value: 60, timescale: 1).displayTime, "1:00", "convert a minute")
+        XCTAssertEqual(CMTimeMake(value: 601, timescale: 1).displayTime, "10:01", "convert ten minutes")
+        XCTAssertEqual(CMTimeMake(value: 3600, timescale: 1).displayTime, "60:00", "convert an hour")
+        XCTAssertEqual(CMTimeMake(value: 86400, timescale: 1).displayTime, "1440:00", "convert a day")
     }
 
     func testExample() {
